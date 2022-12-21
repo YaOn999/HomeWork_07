fd00a8d19aa77e0383423d8b66dab26568b958fe
YaOn999 <lumous999@gmail.com>
2022-12-22 00:12:42 +0300

    add HW_02

---
+++ HW_02/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции 
+// элемента в двумерном массиве, и возвращает значение этого элемента 
+// или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+void PrintArray(int[,] arr, int rows, int columns)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(arr[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    if (rows < 5 && columns < 5) Console.Write("в данной позиции число: " + arr[rows, columns]);
+    else 
+    {
+        Console.Write("такого числа в массиве нет");
+    }
+}
+
+int[,] FillArray(int rows, int columns, int begin, int end)
+{
+    int[,] array = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = new Random().Next(begin, end);
+        }
+    }
+    return array;
+}
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int rows = EnterData("Чтобы найти значение введите номер строки: ");
+int columns = EnterData("введите номер столбца: ");
+
+int[,] matrix = FillArray(5, 5, 0, 100);
+PrintArray(matrix, rows, columns);
